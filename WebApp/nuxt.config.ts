c0089aa83293fe443463f6d552c4ddddcedffbab
Arvin Morawej <arvin.morawej@gmail.com>
2025-02-07 12:39:31 -0500

    Add custom font and CSS styles for the application

--- WebApp/nuxt.config.ts
+++ WebApp/nuxt.config.ts
@@ -3,11 +3,19 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   extends: ['@nuxt/ui-pro'],
-  modules: ['@nuxt/ui',  'v-gsap-nuxt'],
+  modules: ['@nuxt/ui',  'v-gsap-nuxt', '@nuxt/fonts'],
+  css: [
+    '@/assets/css/main.css'
+  ],
   vgsap: {
     presets: [],
     breakpoint: 768,
     scroller: '',
     composable: true
   },
+  fonts: {
+    families: [
+      { name: 'Alata', provider: 'google', global: true, weights: ['400'] },
+    ],
+  }
 })
