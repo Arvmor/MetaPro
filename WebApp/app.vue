0a5e709bee4087cee81feb091c8fdaf15eb0643f
Arvin Morawej <arvin.morawej@gmail.com>
2025-02-07 12:09:09 -0500

    Add initial Nuxt.js application structure with configuration files and components

---
+++ WebApp/app.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { Base } from '#components';
+
+const links = [{
+  label: 'Docs',
+  icon: 'i-heroicons-book-open',
+  to: '/getting-started'
+}, {
+  label: 'Pro',
+  icon: 'i-heroicons-square-3-stack-3d',
+  to: '/pro'
+}, {
+  label: 'Releases',
+  icon: 'i-heroicons-rocket-launch',
+  to: '/releases'
+}]
+
+const ui = {
+  wrapper: "relative text-black",
+};
+</script>
+
+<template>
+  <!-- Header -->
+  <UHeader :links="links" :ui="ui">
+    <!-- Font size bigger -->
+    <template #left>
+      <text class="text-3xl font-bold">MetaPro</text>
+    </template>
+
+    <template #right>
+      <UButton color="primary" variant="solid">Early Access</UButton>
+    </template>
+
+  </UHeader>
+
+  <!-- Main -->
+  <UMain>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </UMain>
+
+  <!-- Footer -->
+  <UFooter :links="links">
+    <template #left>
+      Copyright © {{ new Date().getFullYear() }}
+    </template>
+
+    <template #right>
+      <UButton icon="i-simple-icons-x" color="gray" variant="ghost" to="https://x.com/TheMetaPro" target="_blank" />
+      <UButton icon="i-simple-icons-discord" color="gray" variant="ghost" to="https://discord.gg/p9RZaBPSdF" target="_blank" />
+      <UButton icon="i-simple-icons-github" color="gray" variant="ghost" to="https://github.com/Arvmor/MetaPro" target="_blank" />
+    </template>
+  </UFooter>
+</template>
---
+++ WebApp/pages/index.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+    import { object, string, type InferType } from 'yup'
+    import type { FormSubmitEvent } from '#ui/types'
+
+    const schema = object({
+        email: string().email('Invalid email').required('Required')
+    })
+
+    type Schema = InferType<typeof schema>
+
+    const state = reactive({
+        email: undefined
+    })
+
+    async function onSubmit(event: FormSubmitEvent<Schema>) {
+        // Do something with event.data
+        console.log(event.data)
+    }
+</script>
+
+
+<template>
+    <div>
+        <ULandingSection class="h-screen w-full flex flex-col justify-center items-center ">
+            <template #headline>
+                <div class="text-gray-500 text-3xl">
+                    <span>The crypto wallet that </span> 
+                    <UIcon name="i-fa6-solid-recycle"/>
+                    <span> earnings</span>
+                </div>
+            </template>
+
+            <template #title>
+                <div class="text-primary text-9xl">
+                    <span class="text-black">Meta,</span><span> but this time</span>
+                </div>
+                <div class="text-primary text-9xl">
+                    <span>for </span><span class="text-black">Pros</span>
+                </div>
+            </template>
+
+            <template #description>
+                <UButton color="primary">Join for Airdrop!</UButton>
+                <div class="text-gray-400">
+                    <span>By using this you agree to our <u>Terms of Service</u>.</span>
+                </div>
+            </template>
+        </ULandingSection>
+  
+        <ULandingSection title="The freedom to build anything" align="left" />
+        <ULandingSection title="The flexibility to control your data" align="right" />
+  
+    </div>
+    <ULandingCard
+        title="Newsletter"
+        description="Veniam minim ipsum anim. Irure voluptate magna dolore id dolore ex quis sint sint et duis dolor enim fugiat."
+        icon="i-heroicons-photo"
+        color="primary"
+        class="max-w-lg"
+    >
+        <UForm :schema="schema" :state="state" @submit="onSubmit">
+            <UFormGroup label="Email" name="email">
+                <UInput v-model="state.email" />
+            </UFormGroup>
+        
+            <UButton type="submit">Sign Up</UButton>
+        </UForm>
+    </ULandingCard>
+</template>
+
+  
