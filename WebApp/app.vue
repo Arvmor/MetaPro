53410cec9b284fb2322affe48bae95292187f0a4
Arvin Morawej <arvin.morawej@gmail.com>
2025-02-25 09:24:56 -0500

    feat: Add SEO metadata

--- WebApp/app.vue
+++ WebApp/app.vue
@@ -1,3 +1,34 @@
+<script setup>
+useSeoMeta({
+  title: "MetaPro - Ethereum Private Wallet",
+  description:
+    "MetaPro is a private Ethereum wallet that allows you to store your assets securely.",
+  ogTitle: "Ethereum Private Wallet",
+  ogDescription:
+    "MetaPro is a private Ethereum wallet that allows you to store your assets securely.",
+  ogImage: "/favicon-32x32.png",
+  ogUrl: "https://metapro.app",
+  twitterTitle: "MetaPro - Ethereum Private Wallet",
+  twitterDescription:
+    "MetaPro is a private Ethereum wallet that allows you to store your assets securely.",
+  twitterImage: "/favicon-32x32.png",
+  twitterCard: "summary_large_image",
+});
+
+useHead({
+  htmlAttrs: {
+    lang: "en",
+  },
+  link: [
+    {
+      rel: "icon",
+      type: "image/x-icon",
+      href: "/favicon.ico",
+    },
+  ],
+});
+</script>
+
 <template>
   <!-- Main -->
   <UMain>
